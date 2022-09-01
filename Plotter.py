9f618aea3f62d0023e3d4f3a89b39697d7319a56
elmehdimak <elmehdimak@gmail.com>
2022-09-01 16:39:50 -0400

    add Plotter and stl_path files

---
+++ Plotter.py
@@ -0,0 +1,118 @@
+#!./venv/bin/python
+import json
+import math
+import os
+import sys
+from scipy.integrate import cumtrapz
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+from hdrh.histogram import HdrHistogram
+
+
+class Plotter:
+    def __init__(self, files: dict, CONST_TRAFFIC, CONST_DUT, CMP: bool, **kwargs):
+        self.files = files
+        self.CONST_TRAFFIC = CONST_TRAFFIC
+        self.CONST_DUT = CONST_DUT
+        self.CMP = CMP
+        if CMP:
+            if "CMP_NAME" in kwargs:
+                self.CMP_NAME = kwargs["CMP_NAME"]
+            else:
+                raise TypeError("Must give CMP_NAME value if CMP is set")
+
+        d = self.decode(self.files)
+        self.hdr_histograms = d["hdrh"]
+        self.jitters = d["jitter"]
+
+    def parse_file(self, path):
+        f = open(path, "r")
+        json_data = json.loads(f.read())
+        f.close()
+
+        value_mean = json_data["latency"]["average"]
+        tot_max = json_data["latency"]["total_max"]
+        tot_min = json_data["latency"]["total_min"]
+        jitter = json_data["latency"]["jitter"]
+        histogram = json_data["latency"]["histogram"]
+        hdrh = json_data["latency"]["hdrh"]
+
+        return {
+            "mean": value_mean,
+            "tot_max": tot_max,
+            "tot_min": tot_min,
+            "jitter": jitter,
+            "histogram": histogram,
+            "hdrh": hdrh,
+        }
+
+    def decode(self, files):
+        hs = dict.fromkeys(self.CONST_DUT)
+        js = dict.fromkeys(self.CONST_DUT)
+        for e in hs:
+            hs[e] = {}
+        for e in js:
+            js[e] = {}
+        for file in files:
+            DUT = file.split("/")[1]
+            traffic_type = file.split("/")[2]
+            if (DUT in self.CONST_DUT) and (traffic_type in self.CONST_TRAFFIC):
+                parsed = self.parse_file(file)
+                hdrh = parsed["hdrh"]
+                jitter = parsed["jitter"]
+                h = HdrHistogram.decode(hdrh)
+                hs[DUT][traffic_type] = h
+                js[DUT][traffic_type] = jitter
+        return {"hdrh": hs, "jitter": js}
+
+    def do_plot(self):
+
+        if self.CMP:
+            for traffic in self.CONST_TRAFFIC:
+                outs = ""
+                jitters = []
+                for dut in self.hdr_histograms.keys():
+                    out = "out/{}/{}_{}.txt".format(
+                        dut.strip("."), dut.strip("."), traffic
+                    )
+                    outs += out + " "
+                    self.hdr_histograms[dut][traffic].output_percentile_distribution(
+                        open(out, "wb+"), 1000
+                    )
+                    jitters.append(self.jitters[dut][traffic])
+                print("Wrote: ", outs)
+
+                os.system(
+                    "/export/tmp/Downloads/hdr-plot-master/bin/hdr-plot --output out/CMP_{}.png --title '{}' {} ".format(
+                        f"{traffic}_{self.CMP_NAME}",
+                        traffic,
+                        ",".join((map(str, jitters))),
+                        outs,
+                    )
+                )
+
+        hdrh_files = []
+        jitters = []
+        for dut in self.hdr_histograms.keys():
+            histograms = self.hdr_histograms[dut]
+            for traffic_type in histograms.keys():
+                out = "out/{}/{}.txt".format(dut, traffic_type)
+                print("Wrote: ", out)
+                hdrh_files.append(out)
+                histograms[traffic_type].output_percentile_distribution(
+                    open(out, "wb+"), 1000
+                )
+                jitters.append(self.jitters[dut][traffic_type])
+
+        self.plot_multiple(hdrh_files, jitters)
+
+    def plot_multiple(self, hdrh_files, jitters):
+        for i in range(len(hdrh_files)):
+            file = hdrh_files[i]
+            jitter = jitters[i]
+            cmd = "/export/tmp/Downloads/hdr-plot-master/bin/hdr-plot --output {}.png --title '{}' {} ".format(
+                file.strip(".txt"), file.strip(".txt"), file,
+            )
+            os.system(cmd)
---
+++ stl_path.py
@@ -0,0 +1,17 @@
+import sys, os
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+cur_dir = os.path.dirname(__file__)
+
+#trex_path = f"{config['TREX_LOCATION']}/{config['TREX_VERSION']}"
+trex_path = "/opt/trex/v2.97/"
+interactive = os.path.abspath(f"{trex_path}/automation/trex_control_plane/interactive")
+
+sys.path.insert(0, os.path.abspath(interactive))
+
+STL_PROFILES_PATH = os.path.join(f"{trex_path}/stl")
+EXT_LIBS_PATH = os.path.abspath(f"{trex_path}/external_libs")
+assert os.path.isdir(STL_PROFILES_PATH), "Could not determine STL profiles path"
+assert os.path.isdir(EXT_LIBS_PATH), "Could not determine external_libs path"

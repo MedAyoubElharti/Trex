328a61544a7862cf202600c97a03ba8a42a163b9
elmehdimak <elmehdimak@gmail.com>
2022-09-01 17:50:12 -0400

    first script to inject packets using console-like command

--- stl_start.py
+++ stl_start.py
@@ -7,3 +7,24 @@ import stl_path
 from trex.stl.api import *
 from Plotter import Plotter
 
+#ports
+tx_port = 1
+rx_port = 0
+
+# create client
+c = STLClient()
+
+# connect to server
+c.connect()
+
+#reset ports
+c.reset(ports=[tx_port, rx_port])
+
+
+#inject packets using console-like command
+
+print(
+	"\nInjecting packets on port {0}\n".format(tx_port)
+)
+
+c.start_line("-f /opt/trex/v2.97/stl/AGF_stream/gtp_1pkt_simple_2stream.py -m 20.5gbps -p 1")

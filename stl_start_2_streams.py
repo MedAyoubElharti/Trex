1ac8f6f38d2822240581849f715a261e69b3be41
elmehdimak <elmehdimak@gmail.com>
2022-09-02 12:18:16 -0400

    inject 2 streams

---
+++ stl_start_2_streams.py
@@ -0,0 +1,59 @@
+import stl_path
+from trex_stl_lib.api import *
+
+import time
+import json
+
+
+# create udp packet
+
+def create_udp_pkt(src_ip):
+	
+	return STLPktBuilder(
+		pkt = Ether() / IP(src=src, dst="10.0.0.10") / UDP(dport=1400) / Raw('x'*20)
+	)
+
+def start():
+	
+	# create client
+	c = STLClient()
+	
+	passed = True
+	
+	try:	
+		# create two streams
+
+		s1 = STLStream(packet=create_pkt("10.0.0.1"),
+				mode=STLXCont())
+
+		s1 = STLStream(packet=create_pkt("10.0.0.2"),
+				mode=STLXCont())
+
+		# connect to server
+		c.connect()
+
+		# reset ports
+		c.reset(ports=[0, 1])
+		
+		# add streams to tx_port (1)
+		c.add_streams([s1, s2], ports = [1]) 
+		
+		# clear stats
+		c.clear_stats()
+
+		# set rx_port (0) as promiscuous mode
+		c.set_port_attr(ports=[0], promiscuous=True)
+		
+		# start injecting packets:
+		print("Injecting 10 Mpps on ports 1 for 30s .........")
+		c.strat(ports=[1], mult="10mpps", duration = 30)
+
+		#
+		c.wait_on_traffic(ports=[1])
+
+		# get stats 
+		stats = c.get_stats()
+
+		
+
+
